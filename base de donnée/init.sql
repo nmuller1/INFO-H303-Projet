fc660dd474e42b665674e51d99352e9896002063
loukwets <loukaswets@hotmail.com>
2019-04-29 21:24:59 +0200

    table charger user

--- base de donnée/init.sql
+++ base de donnée/init.sql
@@ -1,8 +1,16 @@
 set timezone='UTC';
 
-CREATE TABLE  user_  (
+CREATE TABLE  user (
    id          INT PRIMARY KEY ,
    pseudo      VARCHAR(20) ,
    password    VARCHAR(64) NOT NULL,
    cardNum        VARCHAR(64) NOT NULL  UNIQUE,
+);
+
+CREATE TABLE  CHARGER_USER  (
+   id        INT PRIMARY KEY REFERENCES user(id) ON DELETE CASCADE,
+   firstname VARCHAR(20) NOT NULL,
+   lastname  VARCHAR(20) NOT NULL,
+   adress    VARCHAR(255) NOT NULL,
+   phoneNum  VARCHAR(20) NOT NULL,
 );
