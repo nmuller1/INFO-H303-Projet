811aa3cbeb38d486aa08a09de0342e0abd0e510e
loukwets <loukaswets@hotmail.com>
2019-05-05 15:17:57 +0200

    erreur id mecanic

--- base de donnée/extractData.py
+++ base de donnée/extractData.py
@@ -1,10 +1,9 @@
 import xml.etree.ElementTree as ET
 output_file = "data.sql"
-
+insert = ""
 
 tree = ET.parse('data2019/anonyme_users.xml')  
 root = tree.getroot()
-insert = ""
 for elem in root:
     insert +="INSERT INTO user_ (id, pseudo,password, cardNum) VALUES "
     insert += "('"+elem[0].text+"','"+''+"','"+elem[1].text+"','"+elem[2].text+"')" + ";\n"
@@ -13,7 +12,7 @@ tree = ET.parse('data2019/mecaniciens.xml')
 root = tree.getroot()
 
 for elem in root:
-    insert +="INSERT INTO charger_user (id, lastname, firstname,password, phoneNum, road, roadNum, codePostal, commune, hireDate, cardNum) VALUES "
+    insert +="INSERT INTO mechanic (id, lastname, firstname,password, phoneNum, road, roadNum, codePostal, commune, hireDate, cardNum) VALUES "
     insert += "('"+elem[0].text+"','"+elem[1].text+"','"+elem[2].text+"','"+elem[3].text+"','"+elem[4].text+"','"+elem[5][0].text+"','"+elem[5][1].text+"','"+elem[5][2].text+"','"+elem[5][3].text+"','"+elem[6].text+"','"+elem[7].text+"')" + ";\n"
 
 
@@ -24,7 +23,8 @@ for elem in root:
     insert +="INSERT INTO user_ (id, pseudo,password, cardNum) VALUES "
     insert += "('"+elem[0].text+"','"+''+"','"+elem[3].text+"','"+elem[6].text+"')" + ";\n"
 
-    insert +="INSERT INTO user_ (id,firstname,lastname,phoneNum,road,roadNum,codePostal,commune) VALUES "
+    insert +="INSERT INTO CHARGER_USER (id,firstname,lastname,phoneNum,road,roadNum,codePostal,commune) VALUES "
     insert += "('"+elem[0].text+"','"+elem[2].text+"','"+elem[1].text+"','"+elem[4].text+"','"+elem[5][0].text+"','"+elem[5][1].text+"','"+elem[5][2].text+"','"+elem[5][3].text+"')" + ";\n"
+
 with open(output_file, 'w') as f :
     f.write(insert)
--- base de donnée/init.sql
+++ base de donnée/init.sql
@@ -11,6 +11,17 @@ CREATE TABLE  CHARGER_USER  (
    id        INT PRIMARY KEY REFERENCES user_(id) ON DELETE CASCADE,
    lastname  VARCHAR(20) NOT NULL,
    firstname VARCHAR(20) NOT NULL,
+   phoneNum  VARCHAR(20) NOT NULL,
+   road    VARCHAR(255) NOT NULL,
+   roadNum    VARCHAR(255) NOT NULL,
+   codePostal    VARCHAR(255) NOT NULL,
+   commune    VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE mechanic(
+   id    DECIMAL PRIMARY KEY ,
+   firstname VARCHAR(20) NOT NULL,
+   lastname  VARCHAR(20) NOT NULL,
    password    VARCHAR(64) NOT NULL,
    phoneNum  VARCHAR(20) NOT NULL,
    road    VARCHAR(255) NOT NULL,
@@ -20,16 +31,3 @@ CREATE TABLE  CHARGER_USER  (
    hireDate    VARCHAR(64) NOT NULL,
    cardNum        VARCHAR(64) NOT NULL
 );
-
-CREATE TABLE mechanic(
-   id int PRIMARY KEY ,
-   firstname VARCHAR(20) NOT NULL,
-   lastname  VARCHAR(20) NOT NULL,
-   phoneNum  VARCHAR(20) NOT NULL,
-   road    VARCHAR(255) NOT NULL,
-   roadNum    VARCHAR(255) NOT NULL,
-   codePostal    VARCHAR(255) NOT NULL,
-   commune    VARCHAR(255) NOT NULL
-);
-
-ALTER TABLE mechanic ALTER COLUMN id TYPE BIGINT;
