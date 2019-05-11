b1a2c7c941e8c7f12ff53831be326926481d369e
loukwets <loukaswets@hotmail.com>
2019-05-11 12:42:06 +0200

    request R5

--- webPage/requetesSQL.py
+++ webPage/requetesSQL.py
@@ -6,7 +6,7 @@ R1 = """SELECT trips.scooter, trips.destinationX, trips.destinationY
         ORDER BY trips.scooter ASC
         """
 
-R2 = """select user_id, count(scooter)              --pas finie
+R2 = """select user_id, count(scooter)              --pas fini
         from (
         select r.user_id user_id, r.scooter scooter from reloads r
         union all
@@ -43,7 +43,11 @@ R4 = """SELECT scooter, COUNT(scooter)
         """
 
 R5 = """ SELECT t.userID, AVG(t.endTime-t.startTime), COUNT(t.userID),
-        SUM(CASE when TO_TIMESTAMP(t.endTime-t.startTime, 'HH24:MI:SS')::TIME) < '01:00:00') THEN EXTRACT(minute FROM t.endTime-t.startTime)*0.15 end as 'okk')
+        SUM(CASE 
+            WHEN EXTRACT(hour FROM t.endTime-t.startTime) < 1 THEN EXTRACT(minute FROM t.endTime-t.startTime)*0.15
+            WHEN EXTRACT(hour FROM t.endTime-t.startTime) > 1 THEN EXTRACT(HOUR FROM t.endTime-t.startTime)*6.5
+            WHEN EXTRACT(DAY FROM t.endTime-t.startTime) > 1 THEN EXTRACT(DAY FROM t.endTime-t.startTime)*36 
+        end)+COUNT(t.userID)
         FROM trips t
         GROUP BY t.userID
         HAVING COUNT(t.userID) > 10
